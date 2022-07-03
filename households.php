c5e502d0a6b257d79f8d9a08a9fe5eb57772ee33
oliver jansen rodriguez <oliverjansen16@gmail.com>
2022-07-03 17:53:21 +0800

    updates

---
+++ households.php
@@ -0,0 +1,235 @@
+<?php include('db_connect.php');?>
+<div class="container-fluid">
+<style>
+	input[type=checkbox]
+{
+  /* Double-sized Checkboxes */
+  -ms-transform: scale(1.5); /* IE */
+  -moz-transform: scale(1.5); /* FF */
+  -webkit-transform: scale(1.5); /* Safari and Chrome */
+  -o-transform: scale(1.5); /* Opera */
+  transform: scale(1.5);
+  padding: 10px;
+}
+</style>
+	<div class="col-lg-12">
+		<div class="row mb-4 mt-4">
+			<div class="col-md-12">
+				
+			</div>
+		</div>
+		<div class="row">
+			<!-- FORM Panel -->
+
+			<!-- Table Panel -->
+			<div class="col-md-12">
+				<div class="text-center mt-4 mb-4">
+					<h2>List of Households</h2>
+				</div>
+				<div class="card">
+					<div class="card-header">
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-md text-center>
+									<span class="">
+										<button class="btn btn-primary btn-sm ml-2 mb-1 float-right"  type="button" id="new_household">
+										<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+										<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+										</svg></i> Add Household</button>
+										<button class="btn btn-success  btn-sm float-right"  type="button" id="print_selected">
+										<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+										<path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+										<path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
+										</svg></i> Print Selected</button>
+									</span>
+								</div>
+							</div>
+						</div>	
+					</div>
+					<div class="card-body table-responsive">
+						<table class="table table-hover table-striped table-bordered">
+							<thead>
+								<tr>
+									<!-- <th class="text-center">
+										 <div class="form-check">
+										  <input class="form-check-input position-static" type="checkbox" id="check_all"  aria-label="...">
+										</div>
+									</th> -->
+									<th class="text-center">Distribute</th>
+									<th class="text-center">#</th>
+									<th class="">Tracking ID</th>
+									<th class="">Name</th>
+									<th class="">Address</th>
+									<th class="text-center">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+								$i = 1;
+								$types = $conn->query("SELECT *,concat(lastname,', ',firstname,' ',middlename) as name, concat(address,', ',street,', ',baranggay,', ',city,', ',state,', ',zip_code) as caddress FROM households order by name asc");
+								while($row=$types->fetch_assoc()):
+								?>
+								<tr>
+									<!-- <th class="text-center">
+										<div class="form-check">
+										 	<input class="form-check-input position-static input-lg" type="checkbox" name="checked[]" value="<?php echo $row['id'] ?>">
+									 	</div>
+									</th> -->
+									<td class="text-center">
+									<button class="btn btn-sm btn-warning distribute_done mt-1" style="width:80px" type="button" data-id="<?php echo $row['id'] ?>" ><i class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+									<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+									</svg></i></button>
+									</td>
+									<td class="text-center"><?php echo $i++ ?></td>
+									<td class="">
+										 <p> <?php echo $row['tracking_id'] ?></p>
+									</td>
+									<td class="">
+										 <p> <?php echo ucwords($row['name']) ?></p>
+									</td>
+									<td class="">
+										 <p> <?php echo $row['caddress'] ?></p>
+									</td>
+									<td class="text-center">
+										
+										<button class="btn btn-sm btn-primary view_household mt-1" style="width:80px" type="button" data-id="<?php echo $row['id'] ?>" >View</button>
+										<button class="btn btn-sm btn-success edit_household mt-1" style="width:80px" type="button" data-id="<?php echo $row['id'] ?>" >Edit</button>
+										<button class="btn btn-sm btn-danger delete_household
+										 mt-1" style="width:80px" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+									</td>
+								</tr>
+								<?php endwhile; ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- Table Panel -->
+		</div>
+	</div>	
+
+</div>
+<style>
+	
+	td{
+		vertical-align: middle !important;
+	}
+	td p{
+		margin: unset
+	}
+	img{
+		max-width:100px;
+		max-height: :150px;
+	}
+</style>
+<script>
+	$(document).ready(function(){
+		$('table').dataTable()
+	})
+	
+	$('#new_household').click(function(){
+		uni_modal("New household","manage_household.php","mid-large")
+	})
+
+	
+	$('.distribute_done').click(function(){
+		distribute_modal("Edits household","manage_distribute.php?id="+$(this).attr('data-id'),"mid-large")
+	})
+
+	$('.edit_household').click(function(){
+		uni_modal("Edit household","manage_household.php?id="+$(this).attr('data-id'),"mid-large")
+		
+	})
+	$('.view_household').click(function(){
+		uni_modal("Household Details","view_household.php?id="+$(this).attr('data-id'),"large")
+		
+	})
+	
+	$('.delete_household').click(function(){
+		_conf("Are you sure to delete this household?","delete_household",[$(this).attr('data-id')])
+	})
+	
+	$('#check_all').click(function(){
+		if($(this).prop('checked') == true)
+			$('[name="checked[]"]').prop('checked',true)
+		else
+			$('[name="checked[]"]').prop('checked',false)
+	})
+
+	$('[name="checked[]"]').click(function(){
+		var count = $('[name="checked[]"]').length
+		var checked = $('[name="checked[]"]:checked').length
+		if(count == checked)
+			$('#check_all').prop('checked',true)
+		else
+			$('#check_all').prop('checked',false)
+	})
+
+	$('#print_selected').click(function(){
+		var checked = $('[name="checked[]"]:checked').length
+		if(checked <= 0){
+			alert_toast("Check atleast one individual details row first.","danger")
+			return false;
+		}
+		var ids = [];
+		$('[name="checked[]"]:checked').each(function(){
+			ids.push($(this).val())
+		})
+
+		start_load()
+		$.ajax({
+			url:"print_households.php",
+			method:"POST",
+			data:{ids : ids},
+			success:function(resp){
+				if(resp){
+					var nw = window.open("","_blank","height=600,width=900")
+					nw.document.write(resp)
+					nw.document.close()
+					nw.print()
+					setTimeout(function(){
+						nw.close()
+						end_load()
+					},700)
+				}
+			}
+		})
+	})
+	
+	function delete_household($id){
+		start_load()
+		$.ajax({
+			url:'function.php?action=delete_household',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("household successfully deleted",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
+
+	function distribute_done($id){
+		start_load()
+		$.ajax({
+			url:'function.php?action=distribute_done',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Relief Packs successfully D",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
+	
+</script>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '984db0214548f877d709b739ddb3cad59bd4a09a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '984db0214548f877d709b739ddb3cad59bd4a09a',
+            'dev_requirement' => false,
+        ),
+        'picqer/php-barcode-generator' => array(
+            'pretty_version' => 'v2.2.3',
+            'version' => '2.2.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../picqer/php-barcode-generator',
+            'aliases' => array(),
+            'reference' => '7b053adfd78b1ee1bbc4a926f56c2b18104188ff',
+            'dev_requirement' => false,
+        ),
+    ),
+);
